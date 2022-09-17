882d921515b8858bf6d1caae53edea619bb8ff1d
Mario <57414832+MaRio0607@users.noreply.github.com>
2022-09-17 15:11:15 -0500

    log in primera version

--- PhoneApp.c
+++ PhoneApp.c
@@ -12,23 +12,17 @@ void nuevo_num(){
     printf("\n\tGenero: " );
     char gen[20];
     scanf("%s",gen);
-    printf("\n\tNumero: ");
+        printf("\n\tNumero: ");
     char num[20];
     scanf("%s",num);
-        printf("\n\tNumero2: ");
-    char num2[20];
-    scanf("%s",num2);
     printf("\n\tEmail: " );
     char email[20];
     scanf("%s",email);
-    printf("\n\tEmail2: " );
-    char email2[20];
-    scanf("%s",email2);
-    fprintf(fp,"%s %s %d %d %s %s\n",nombre,gen,num,num2,email,email2);
+    fprintf(fp,"%s %s %d %s\n",nombre,gen,num,email);
     fclose(fp);
     printf("\n\tPresione alguna tecla para continuar");
     getch();
-    main();
+    Application();
 }
 
 void borrar_num(){
@@ -40,27 +34,26 @@ void borrar_num(){
     printf("\n\t Nombre que desea borrar: ");
     char nombre[20];
         scanf("%s",nombre);
-    char nombreSe[20],gen[20],email[20],email2[20];
-    int num,num2;
-        while(fscanf(fp,"%s %s %d %d %s %s",nombreSe,gen,num,num2,email,email2)!=EOF){
+    char nombreSe[20],gen[20],num[20],email[20];
+        while(fscanf(fp,"%s %s %d %s",nombreSe,gen,num,email)!=EOF){
             if(strcmp(nombre,nombreSe)==0){
                 continue;
             }
-            fprintf(fp1,"%s %s %d %d %s %s\n",nombreSe,gen,num,num2,email,email2);
+            fprintf(fp1,"%s %s %d %s\n",nombreSe,gen,num,email);
         }
     fclose(fp);
     fclose(fp1);
     fp=fopen("info.txt","w");
     fp1=fopen("temp.txt","r");
-        while(fscanf(fp1,"%s %s %d %d %s %s",nombreSe,gen,num,num2,email,email2)!=EOF){
-            fprintf(fp,"%s %s %d %d %s %s",nombreSe,gen,num,num2,email,email2);
+        while(fscanf(fp1,"%s %s %d %s",nombreSe,gen,num,email)!=EOF){
+            fprintf(fp,"%s %s %d %s\n",nombreSe,gen,num,email);
         }
     fclose(fp);
     fclose(fp1);
     remove("temp.txt");
     printf("\n\tPresione alguna tecla para continuar");
     getch();
-    main();
+    Application();
 }
 
 void buscar_num(){
@@ -71,28 +64,24 @@ void buscar_num(){
     printf("\n\t Nombre que desea Buscar: ");
     char name[20];
     scanf("%s",name);
-    char nombreSe[20],gen[20],email[20],email2[20];    
-    int num,num2;
-    while(fscanf(fp,"%s %s %d %d %s %s",nombreSe,gen,num,num2,email,email2)!=EOF){
+    char nombreSe[20],gen[20],num[20],email[20];
+    while(fscanf(fp,"%s %s %d %s",nombreSe,gen,num,email)!=EOF){
         if(strcmp(name,nombreSe)==0){
             printf("\n\tNombre:  %s\n",nombreSe);
             printf("\n\tGenero:  %s\n",gen);
             printf("\n\tNumero:  %d\n",num);
             printf("\n\tEmail:  %s\n",email);
-              printf("\n\tNumero2:  %d\n",num2);
-            printf("\n\tEmail2:  %s\n",email2);
         }
     }
     fclose(fp);
     printf("\n\tPresione alguna tecla para continuar");
     getch();
-    main();
+    Application();
 }
 
-void main(){
-    
+void Application(){
     system("cls");
-    printf("\t============Telefono De Consola===============\n");
+    printf("\n\t============Telefono De Consola===============\n");
     printf("\t 1. Agregar Contacto\n");
     printf("\t 2. Buscar Contacto\n");
     printf("\t 3. Borrar Contacto\n");
@@ -111,7 +100,73 @@ void main(){
             borrar_num();
             break;
         default:
+            Application();
+    }
+}
+
+void Login(){
+    FILE *fp;
+    fp=fopen("password.txt","r");
+    system("cls");
+    printf("\t============Inicio de Sesion===============");
+    printf("\n\tIngrese Usuario: ");
+    char usuario[20];
+    scanf("%s",usuario);
+    // printf("\n\tIngrese Contras: ");
+    // char contra[20];
+    // scanf("%s",contra);
+    char usSE[20],contraSE[20];
+    while(fscanf(fp,"%s",usSE)!=EOF){
+        if(strcmp(usuario,usSE)==0 ){  
+            fclose(fp);
+            printf("\n\tPresione alguna tecla para ir a la applicacion");
+            getch();
+            Application();
+        
+        }else{
+            fclose(fp);
+            printf("\n\tINCORRECTO Presione alguna tecla para continuar");
+            getch();
             main();
+        }
     }
 
+}
+
+void Register(){
+FILE *fp;
+    fp=fopen("password.txt","a+");
+    system("cls");
+    printf("\t============Nuevo Usuario===============");
+    printf("\n\tUsuario: ");
+    char Usuario[20];
+    scanf("%s",Usuario);
+    printf("\n\tContra: " );
+    char Contra[20];
+    scanf("%s",Contra);
+    fprintf(fp,"%s %s\n",Usuario,Contra);
+    fclose(fp);
+    printf("\n\tPresione alguna tecla para continuar");
+    getch();
+    main();
+}
+
+void main(){
+        system("cls");
+    printf("\t============Desea registrarse o iniciar sesion===============\n");
+    printf("\t 1. Iniciar Sesion\n");
+    printf("\t 2. Resgistrar Usuario\n");
+    printf("Elija una opcion: ");
+    int select;
+    scanf("%d",&select);
+    switch (select){
+        case 1:
+            Login();
+            break;
+        case 2:
+            Register();
+            break;
+        default:
+            main();
+    }
 }
